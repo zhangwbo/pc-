bee40d74046dfc4cbc0d235a5778432e670c67c0
zhangwenbo <2677317002@qq.com>
2019-01-08 09:47:45 +0800

    完成第五屏样式和结构

--- js/index.js
+++ js/index.js
@@ -11,6 +11,14 @@ window.onload = function () {
     var nowIndex = 0;
     var wheelTimer = null;
 
+  var carouselLiNodes = document.querySelectorAll('.home-carousel li');
+  var pointLiNodes = document.querySelectorAll('.home-point li');
+  var homeNode = document.querySelector('.Home');
+
+  var timer = null;
+  var lastIndex = 0;
+  var lastTime = 0;
+
     //头部JS
     header();
     function header() {
@@ -29,7 +37,7 @@ window.onload = function () {
     }
 
     //公共移动函数
-    move(3);
+    move(1);
     function move(nowIndex) {
         for (var j = 0; j < downNodes.length; j++) {
             downNodes[j].style.width = '0';
@@ -94,15 +102,6 @@ window.onload = function () {
         contentList.style.top = -nowIndex * contentHeight + 'px';
     }
 
-    //获取
-    var carouselLiNodes = document.querySelectorAll('.home-carousel li');
-    var pointLiNodes = document.querySelectorAll('.home-point li');
-    var homeNode = document.querySelector('.Home');
-
-    var timer = null;
-    var lastIndex = 0;
-    var lastTime = 0;
-    var nowIndex = 0;
     //轮播图
     for (var i = 0; i < pointLiNodes.length; i++) {
 
@@ -157,6 +156,130 @@ window.onload = function () {
             lastIndex = nowIndex;
         }, 2000)
     }
+
+    //第五屏
+  lastView();
+  function lastView() {
+    var teamUlNode = document.querySelector('.team-list');
+    var teamLiNodes = document.querySelectorAll('.team-list li');
+
+    var width = teamLiNodes[0].offsetWidth;
+    var height = teamLiNodes[0].offsetHeight;
+
+    var canvas = null;
+    var createArcTimer = null;
+    var paintingTimer = null;
+    //给所有LI绑定移入事件
+    for (var i = 0; i < teamLiNodes.length; i++) {
+      teamLiNodes[i].index = i;
+      teamLiNodes[i].onmouseenter = function () {
+        for (var j = 0; j < teamLiNodes.length; j++) {
+          teamLiNodes[j].style.opacity = 0.5;
+        }
+        this.style.opacity = 1;
+
+        if(!canvas){
+          canvas = document.createElement('canvas');
+          canvas.width = width;
+          canvas.height = height;
+          canvas.className = 'myCanvas';
+          bubble(canvas);
+          teamUlNode.appendChild(canvas);
+        }
+        canvas.style.left = this.index * width +'px';
+
+      }
+    }
+    //给UL添加移出事件
+    teamUlNode.onmouseleave = function () {
+      for (var i = 0; i < teamLiNodes.length; i++) {
+        teamLiNodes[i].style.opacity = 1;
+      }
+      canvas.remove();
+      canvas = null;
+      clearInterval(createArcTimer);
+      clearInterval(paintingTimer);
+
+    }
+  }
+    //气泡运动函数
+  function bubble(canvas) {
+
+
+    var ctx = canvas.getContext('2d');
+    var width = canvas.width;
+    var height = canvas.height;
+
+    var arr = [];
+
+    //生成圆
+    createArcTimer = setInterval(function () {
+
+
+      var r = Math.round(Math.random()*255);
+      var g = Math.round(Math.random()*255);
+      var b = Math.round(Math.random()*255);
+
+      var c_r = Math.round(Math.random()*8+2);
+      var s = Math.round(Math.random()*30+20);
+
+      var x = Math.round(Math.random()*width);
+      var y = height+c_r;
+      arr.push({
+        x:x,
+        y:y,
+        r:r,
+        g:g,
+        b:b,
+        deg:0,
+        c_r:c_r,
+        s:s
+      })
+
+    },80);
+
+    //画圆
+    paintingTimer = setInterval(function () {
+      ctx.clearRect(0,0,width,height);
+
+      for (var i = 0; i < arr.length; i++) {
+        var item = arr[i];
+        item.deg += 4;
+        var rad = item.deg * Math.PI / 180;
+        var x = item.x + Math.sin(rad) * item.s;
+        var y = item.y - rad*item.s;
+
+        if(y <= -item.c_r){
+          arr.splice(i,1);
+          continue;
+        }
+        ctx.fillStyle = 'rgb(' + item.r + ',' + item.g + ',' + item.b + ')';
+        ctx.beginPath();
+        ctx.arc(x,y,item.c_r,0,2*Math.PI);
+        ctx.fill();
+      }
+    },1000/60)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
